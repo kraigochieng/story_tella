bbea2ac0c51724a2ef8745591f91d2c99e591e02
Kalundaah <84059634+kalundaah@users.noreply.github.com>
2025-01-23 00:18:23 +0300

    Sentiment analysis

--- server/sentiment/__init__.py
+++ server/sentiment/__init__.py
@@ -1,18 +1,23 @@
-from flask import Blueprint,request
+from flask import Blueprint,request,jsonify
 sentiment = Blueprint("sentiment", __name__)
 
-from server.sentiment.chat import chat
+from server.sentiment.chat import chat,model
 
 @sentiment.route("/", methods=["POST"])
 def get_sentiment():
     try:
-        instance = chat()
+        instance = chat(model)
         if request.method == 'POST':
             paragraphs = request.form['data']
-            for paragraph in paragraphs:
-                chat.prompt(paragraph)
+            chunk_size = 2000
+            paragraphs = [paragraphs[i:i + chunk_size] for i in range(0, len(paragraphs), chunk_size)]
+            for row in paragraphs:
+                instance.prompt(row)
+            
+            
+            data= instance.get_results()
         
-        return {"data":f"{instance.get_results()}"}
+            return {"data":f"{instance.get_results()}"}
     except Exception as e:
-        return {"data":e}
+        return jsonify({"error": str(e)}), 500
 
